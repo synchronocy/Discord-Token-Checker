87e97d83cc4c253270a35bef77b46302e53dbc7c
blakeando <blakeando@live.com>
2019-02-15 03:17:19 +1100

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,19 @@
 # Simple token checker
 # IDLE Python 3.6 64-bit
 import requests
-headers={
-    'Authorization': 'tokenhere' # I'll soon be adding support for bulk checking.
-}
-src = requests.get('https://discordapp.com/api/v6/auth/login', headers=headers)
-try:
-    if src.status_code == 200:
-        print('Token Works!')
-    else:
-        print('Invalid Token.')
-except Exception:
-    print("Yeah we can't contact discordapp.com")    
+
+with open('tokens.txt','r') as handle:
+        tokens = handle.readlines()
+        for x in tokens:
+            token = x.rstrip()
+            headers={
+                'Authorization': token
+                }
+            src = requests.get('https://discordapp.com/api/v6/auth/login', headers=headers)
+            try:
+                if src.status_code == 200:
+                    print('Token Works!')
+                else:
+                    print('Invalid Token.')
+            except Exception:
+                print("Yeah we can't contact discordapp.com")
